21ad2c42042d2ddd3a59c216f4985cee4d27923a
stationAstral <cougarjonez@gmail.com>
2023-10-26 01:10:23 -0700

    1.3

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -1,13 +1,15 @@
 //
 //  ContentView.swift
 //  YouAreAwesome
-//
 //  Created by stationAstral on 10/25/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString = "You Are Loved"
+    
     var body: some View {
         VStack {
             Image(systemName: "swift")
@@ -16,18 +18,21 @@ struct ContentView: View {
                 .foregroundStyle(.orange)
                 .background(.blue)
                 .padding()
-            Text("You Are Awesome!")
+            
+            Text(messageString)
                 .font(.title2)
                 .fontWeight(.medium)
                 .foregroundStyle(.blue)
                 .kerning(3)
                 .background(.orange)
-              
+            
+            Button("Press Me"){
+                messageString = "You Are Awesome!"
+            }
+            .padding()
         }
-        .padding()
     }
 }
-
 
 #Preview {
     ContentView()
