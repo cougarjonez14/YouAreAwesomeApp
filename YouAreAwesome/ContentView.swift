f438fb74f204a4368f4cfc89c1d6bb4714e7fb2f
stationAstral <cougarjonez@gmail.com>
2023-10-27 11:21:50 -0700

    1.7

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,51 +16,60 @@ struct ContentView: View {
                 .fill(Gradient(colors: [.indigo, .gold]))
                 .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
                 .ignoresSafeArea()
-
-            VStack {
-                Spacer()
-                Text("First App")
-                    .fontWeight(.black)
-                    .font(.largeTitle)
-                    .foregroundStyle(.indianOcean)
-                    .frame(height:100)
-                    .frame(maxWidth: .infinity)
-                    .background(.puebloSand)
-                    .cornerRadius(15)
-                    .padding(.horizontal, 10)
-
-                Spacer()
-                Image(systemName: "swift")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.indianOcean)
-                    .background(.puebloSand)
-                    .cornerRadius(15)
-                    .padding(.horizontal, 35)
-                
-                Spacer()
-                Text(messageString)
-                    .font(.title2)
-                    .fontWeight(.medium)
-                    .minimumScaleFactor(0.5)
-                    .foregroundStyle(.indianOcean)
-                    .kerning(3)
-                    .frame(height: 75)
-                    .frame(maxWidth: .infinity)
-                    .background(.puebloSand)
-                    .cornerRadius(15)
-                    .padding(.horizontal, 10)
-                
-                Spacer()
-                HStack{
+            
+            Group{
+                VStack {
+                    Text("First App")
+                        .fontWeight(.black)
+                        .font(.largeTitle)
+                        .foregroundStyle(.indianOcean)
+                        .frame(height:100)
+                        .frame(maxWidth: .infinity)
+                        .background(.puebloSand)
+                        .cornerRadius(15)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 20)
                     
-                    Button("Press Me"){
-                        messageString = "You Are Awesome!"
-
+                    Divider()
+                        .background(.black)
+                        .padding(.vertical, 20)
+                    Image(systemName: "swift")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.indianOcean)
+                        .background(.puebloSand)
+                        .cornerRadius(15)
+                        .padding(.horizontal, 35)
+                        .padding(.vertical, 20)
+                    
+                    Divider()
+                        .background(.black)
+                        .padding(.vertical, 20)
+                    
+                    Text(messageString)
+                        .font(.title2)
+                        .fontWeight(.medium)
+                        .minimumScaleFactor(0.5)
+                        .foregroundStyle(.indianOcean)
+                        .kerning(3)
+                        .frame(height: 75)
+                        .frame(maxWidth: .infinity)
+                        .background(.puebloSand)
+                        .cornerRadius(15)
+                        .padding(.horizontal, 10)
+                    Divider()
+                        .background(.black)
+                        .padding(.vertical, 20)
+                    
+                    HStack{
+                        
+                        Button("Press Me"){
+                            messageString = "You Are Awesome!"
+                            
+                        }
                     }
-                               }
-                Spacer()
-
+                    
+                }
             }
         }
     }
