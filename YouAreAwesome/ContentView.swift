46d45eee46df211862501ca79d68dc0eb7b005e5
stationAstral <cougarjonez@gmail.com>
2023-10-27 10:48:23 -0700

    1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,32 +11,57 @@ struct ContentView: View {
     @State private var messageString = "You Are Loved"
     
     var body: some View {
-        VStack (spacing: 35){
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-                .background(.blue)
-                .padding()
-            
-            Text(messageString)
-                .font(.title2)
-                .fontWeight(.medium)
-                .minimumScaleFactor(0.5)
-                .foregroundStyle(.blue)
-                .kerning(3)
-                .frame(height: 75)
-                .frame(maxWidth: .infinity)
-                .background(.orange)
-                .padding()
+        ZStack {
+            Rectangle()
+                .fill(Gradient(colors: [.indigo, .gold]))
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea()
 
-            
-            
-            Button("Press Me"){
-                messageString = "You Are Awesome!"
+            VStack {
+                Spacer()
+                Text("First App")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundStyle(.indianOcean)
+                    .frame(height:100)
+                    .frame(maxWidth: .infinity)
+                    .background(.puebloSand)
+                    .cornerRadius(15)
+                    .padding(.horizontal, 10)
+
+                Spacer()
+                Image(systemName: "swift")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.indianOcean)
+                    .background(.puebloSand)
+                    .cornerRadius(15)
+                    .padding(.horizontal, 35)
+                
+                Spacer()
+                Text(messageString)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                    .minimumScaleFactor(0.5)
+                    .foregroundStyle(.indianOcean)
+                    .kerning(3)
+                    .frame(height: 75)
+                    .frame(maxWidth: .infinity)
+                    .background(.puebloSand)
+                    .cornerRadius(15)
+                    .padding(.horizontal, 10)
+                
+                Spacer()
+                HStack{
+                    
+                    Button("Press Me"){
+                        messageString = "You Are Awesome!"
+
+                    }
+                               }
+                Spacer()
+
             }
-            .padding()
-            
         }
     }
 }
