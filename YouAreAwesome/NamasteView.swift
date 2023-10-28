42d3fc81d8dd101f8a277dff55b6facaf4e63bd1
stationAstral <cougarjonez@gmail.com>
2023-10-28 10:04:18 -0700

    1.8

---
+++ YouAreAwesome/NamasteView.swift
@@ -0,0 +1,38 @@
+//
+//  SwiftUIView.swift
+//  YouAreAwesome
+//
+//  Created by stationAstral on 10/28/23.
+//
+
+import SwiftUI
+
+struct NamasteView: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "figure.mind.and.body")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.puebloSand)
+                .padding(40)
+                .background(.indianOcean)
+                .clipShape(Circle())
+                .shadow(color: .indianOcean, radius: 25)
+                .overlay(
+                    Circle()
+                        .stroke(Color.gold, lineWidth: 12)
+                )
+                .padding(20)
+            Text("Namaste")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundColor(.gold)
+                .multilineTextAlignment(.center)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    NamasteView()
+}
