bc6a123955f131a6392e2cba1dd479cca101d1b5
stationAstral <cougarjonez@gmail.com>
2023-10-29 10:08:00 -0700

    1.11.1

--- YouAreAwesome/NamasteView.swift
+++ YouAreAwesome/NamasteView.swift
@@ -6,30 +6,44 @@
 //
 
 import SwiftUI
+//add background for entire view
+
 
 struct NamasteView: View {
     var body: some View {
-        VStack{
-            Image(systemName: "figure.mind.and.body")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(.puebloSand)
-                .padding(40)
-                .background(.indianOcean)
-                .clipShape(Circle())
-                .shadow(color: .indianOcean, radius: 25)
-                .overlay(
-                    Circle()
-                        .stroke(Color.gold, lineWidth: 12)
-                )
-                .padding(20)
-            Text("Namaste")
-                .font(.title)
-                .fontWeight(.black)
-                .kerning(3)
-                .foregroundColor(.gold)
-                .multilineTextAlignment(.center)
-                .padding()
+        ZStack {
+            Rectangle()
+                .fill(Gradient(colors: [.indigo, .gold]))
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                Image(systemName: "figure.mind.and.body")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.puebloSand)
+                
+                    .padding(40)
+                    .background(.indianOcean)
+                    .clipShape(Circle())
+                    .shadow(color: .indianOcean, radius: 25)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.gold, lineWidth: 12)
+                    )
+                    .padding(20)
+                Text("Namaste")
+                    .font(.title)
+                    .padding()
+                    .fontWeight(.black)
+                    .kerning(3)
+                    .foregroundColor(.gold)
+                    .background(.indigo.opacity(0.85))
+                    .cornerRadius(25)
+                    .multilineTextAlignment(.center)
+                    .padding(30)
+            }
         }
     }
 }
